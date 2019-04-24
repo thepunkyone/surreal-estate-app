9e568b712edb9a51728f4052a528553ba513814a
Vitnija Bluzma <thepunkyone@gmail.com>
2019-04-24 13:19:38 +0100

    changed AddProperty component to class, added form and submit button

--- src/components/add-property.jsx
+++ src/components/add-property.jsx
@@ -1,5 +1,30 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import '../style/add-property.css';
 
-const AddProperty = () => <Fragment>Add Property Page</Fragment>;
+class AddProperty extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fields: {
+        title: '',
+      },
+    };
+  }
+
+  handleAddProperty = (event) => {
+    event.preventDefault();
+    console.log(this.state.fields);
+  };
+
+  render() {
+    return (
+      <div className="add-property">
+        <form onSubmit={this.handleAddProperty}>
+          <button type="sumbit">Add</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default AddProperty;
