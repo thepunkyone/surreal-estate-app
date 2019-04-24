14aa2a88ab931c0718baa81b9a51e108b35e8b90
Vitnija Bluzma <thepunkyone@gmail.com>
2019-04-24 11:56:38 +0100

    added react-router-dom to project dependencies

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import 'raf/polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import App from './components/app';
 import './style/app.css';
 
-render(<App />, document.getElementById('root'));
+render(<Router><App /></Router>, document.getElementById('root'));
