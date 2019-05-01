a4b15048b35c8dc68035d626691e2eb9daabb3e4
Vitnija Bluzma <thepunkyone@gmail.com>
2019-05-01 16:48:12 +0100

    changed Properties component to stateful component and added get request

--- src/components/properties.jsx
+++ src/components/properties.jsx
@@ -1,18 +1,49 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
+import axios from 'axios';
 import PropertyCard from './property-card';
+import Alert from './alert';
 
-const Properties = () => (
-  <Fragment>
-    <PropertyCard
-      title="A quirky little house!"
-      type="Semi-Detached"
-      bathrooms="1"
-      bedrooms="3"
-      price="210000"
-      city="Manchester"
-      email="thepunkyone@hotmail.com"
-    />
-  </Fragment>
-);
+class Properties extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      properties: [],
+      isError: false,
+    };
+  }
+
+  componentDidMount() {
+    const url = 'http://localhost:3000/api/v1/PropertyListing/';
+    axios.get(url)
+      .then(({ data }) => {
+        this.setState({ ...this.state, properties: data });
+      })
+      .catch(() => {
+        this.setState({ ...this.state, isError: true });
+      });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        {this.state.isError && <Alert message="Server error! Could not retrieve properties." />}
+        {this.state.properties.map(property => {
+          return (
+            <PropertyCard
+              key={property._id}
+              title={property.title}
+              type={property.type}
+              bathrooms={property.bathrooms}
+              bedrooms={property.bedrooms}
+              price={property.price}
+              city={property.city}
+              email={property.email}
+            />
+          );
+        })}
+      </Fragment>
+    );
+  }
+};
 
 export default Properties;
