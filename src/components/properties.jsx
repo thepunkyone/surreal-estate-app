fbac2bedb5c777ae1378c79fe2d432f27b8d752c
Vitnija Bluzma <thepunkyone@gmail.com>
2019-05-04 00:57:27 +0100

    added filter by city and update style

--- src/components/properties.jsx
+++ src/components/properties.jsx
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import PropertyCard from './property-card';
 import Alert from './alert';
@@ -25,19 +26,41 @@ class Properties extends Component {
       });
   }
 
+  componentDidUpdate(prevProps) {
+    const { search } = this.props.location;
+    if (prevProps.location.search !== search) {
+      axios.get(`${apiUrl}/PropertyListing${search}`)
+        .then(({ data: properties }) => {
+          this.setState({ properties });
+        })
+        .catch(() => {
+          this.setState({ isError: true });
+        });
+    }
+  }
+
   render() {
     return (
       <Fragment>
         {this.state.isError && <Alert message="Server error! Could not retrieve properties." />}
-        <div className="properties">
-          {this.state.properties.map(property => {
-            return (
-              <PropertyCard
-                key={property._id}
-                {...property} //use of spread operator from walkthrough
-              />
-            );
-          })}
+        <div className="flex-wrapper">
+          <div className="city-search">
+            <span>Filter by city</span>
+            <Link to={`/?query={"city": "Manchester"}`}>Manchester</Link>
+            <Link to={`/?query={"city": "Leeds"}`}>Leeds</Link>
+            <Link to={`/?query={"city": "Sheffield"}`}>Sheffield</Link>
+            <Link to={`/?query={"city": "Liverpool"}`}>Liverpool</Link>
+          </div>
+          <div className="properties">
+            {this.state.properties.map(property => {
+              return (
+                <PropertyCard
+                  key={property._id}
+                  {...property} //use of spread operator from walkthrough
+                />
+              );
+            })}
+          </div>
         </div>
       </Fragment>
     );
