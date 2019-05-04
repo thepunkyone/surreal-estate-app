a07a54b17e8f86715f50f65c88981970c874f5f7
Vitnija Bluzma <thepunkyone@gmail.com>
2019-05-04 22:33:18 +0100

    added style

--- src/components/nav-bar.jsx
+++ src/components/nav-bar.jsx
@@ -9,6 +9,10 @@ import { faIgloo } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faIgloo);
 
+const responseFacebook = (response) => {
+  console.log(response);
+};
+
 
 const NavBar = () => {
   return (
@@ -20,7 +24,14 @@ const NavBar = () => {
       <div className="nav">
         <Link className="item" to="/">View Properties</Link>
         <Link className="item" to="/add-property">Add a Property</Link>
-        <Link className="item">Sign In With Facebook</Link>
+        <FacebookLogin
+          appId="1088597931155576"
+          autoLoad
+          fields="name,email,picture"
+          callback={responseFacebook}
+        >
+          Sign In With Facebook
+        </FacebookLogin>
       </div>
     </div>
   );
