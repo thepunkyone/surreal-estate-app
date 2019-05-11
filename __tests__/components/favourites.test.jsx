c9c90797a09537b35ba48c8922e69ce6758267e4
Vitnija Bluzma <thepunkyone@gmail.com>
2019-05-11 21:47:51 +0100

    added getter test

--- __tests__/components/favourites.test.jsx
+++ __tests__/components/favourites.test.jsx
@@ -1,34 +1,43 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import axios from 'axios';
 import Favourites from '../../src/components/favourites';
 
-const mockFavourites = [
-  {
-    _id: '1234',
-    propertyListing: '2345',
-    fbUserId: '3456',
+const mockFavourites = {
+  data: [
+    {
+      _id: '1234',
+      propertyListing: '2345',
+      fbUserId: '3456',
 
-  },
-  {
-    _id: '6776',
-    propertyListing: '5432',
-    fbUserId: '3456',
-  },
-  {
-    _id: '0987',
-    propertyListing: '8765',
-    fbUserId: '5678',
-  },
-];
+    },
+    {
+      _id: '6776',
+      propertyListing: '5432',
+      fbUserId: '3456',
+    },
+    {
+      _id: '0987',
+      propertyListing: '8765',
+      fbUserId: '5678',
+    },
+  ],
+};
+
+jest.mock('axios');
+const getPropertiesMock = Promise.resolve(mockFavourites);
+axios.get.mockImplementation(() => getPropertiesMock);
 
 describe('Favourites Component', () => {
   let wrapper;
   beforeEach(() => {
     wrapper = shallow((
-      <Favourites favourites={mockFavourites} userId={3456} />
+      <Favourites userId="3456" />
     ));
   });
   it('Renders only favourites which correspond to userId', () => {
-    expect(wrapper.find('.favourite')).toHaveLength(2);
+    getPropertiesMock.then(() => {
+      expect(wrapper.find('.favourite')).toHaveLength(2);
+    });
   });
 });
